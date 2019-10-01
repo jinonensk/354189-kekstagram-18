335afeacff51f7f1bbad4af306599c52945c3105
jinonensk <jinonensk@gmail.com>
2019-10-02 01:18:09 +0700

    add pictures to the page

---
+++ js/main.js
@@ -0,0 +1,89 @@
+'use strict';
+var OBJECT_QUANTITY = 25;
+var AVATARS_QUANTITY = 6;
+var MAX_COMMENTS_QUANTITY = 5;
+var MAX_LIKES = 200;
+var MIN_LIKES = 15;
+var NAMES = ['Саша', 'Костя', 'Даша', 'Марина', 'Ирина', 'Василий'];
+var DESCRIPTIONS = [
+  'Фото 1',
+  'Какое-то фоток 2',
+  'Наверное фоток 3',
+  'А это фоток 4',
+  'Не совсем понятно',
+  'Что с описаниями этими'
+];
+var COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+var pictureTemplate = document.querySelector('#picture')
+    .content
+    .querySelector('.picture');
+
+var fragment = document.createDocumentFragment();
+var picturesSection = document.querySelector('.pictures');
+
+var getRandomValue = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+var makeComment = function () {
+  var nameIndex = getRandomValue(0, NAMES.length - 1);
+  var commentIndex = getRandomValue(0, COMMENTS.length - 1);
+  var avatarIndex = getRandomValue(1, AVATARS_QUANTITY);
+  var comment = {
+    avatar: 'img/avatar-' + avatarIndex + '.svg',
+    message: COMMENTS[commentIndex],
+    name: NAMES[nameIndex],
+  };
+  return comment;
+};
+
+var makeComments = function () {
+  var commentsArray = [];
+  var randomCommensQuantity = getRandomValue(1, MAX_COMMENTS_QUANTITY);
+  for (var i = 1; i <= randomCommensQuantity; i++) {
+    var newComment = makeComment();
+    commentsArray.push(newComment);
+  }
+  return commentsArray;
+};
+
+var makePicturesArray = function () {
+  var picturesArray = [];
+  for (var i = 1; i <= OBJECT_QUANTITY; i++) {
+    var picture = {
+      url: 'photos/' + i + '.jpg',
+      description: getRandomValue(0, DESCRIPTIONS.length - 1),
+      likes: getRandomValue(MIN_LIKES, MAX_LIKES),
+      commets: makeComments(),
+    };
+    picturesArray.push(picture);
+  }
+  return picturesArray;
+};
+
+var renderPicture = function (picture) {
+  var newPicture = pictureTemplate.cloneNode(true);
+  newPicture.querySelector('.picture__img').src = picture.url;
+  newPicture.querySelector('.picture__comments').textContent = picture.commets.length;
+  newPicture.querySelector('.picture__likes').textContent = picture.likes;
+  return newPicture;
+};
+
+var addPicturesToPage = function () {
+  var pictures = makePicturesArray();
+  for (var i = 0; i < pictures.length; i++) {
+    fragment.appendChild(renderPicture(pictures[i]));
+  }
+  picturesSection.appendChild(fragment);
+};
+
+addPicturesToPage();
+
