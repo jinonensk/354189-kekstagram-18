406a63b92937c4cf4b20420b12382a3d582d4e7e
jinonensk <jinonensk@gmail.com>
2019-10-22 03:31:54 +0700

    add server load and comments habdle

--- js/gallery/gallery.js
+++ js/gallery/gallery.js
@@ -2,18 +2,24 @@
 
 (function () {
   var utils = window.utils;
+  var dbRequests = window.dbRequests;
   var pictureTemplate = document.querySelector('#picture')
-      .content
-      .querySelector('.picture');
+    .content
+    .querySelector('.picture');
   var picturesSection = document.querySelector('.pictures');
 
   var renderPicture = function (picture) {
     var newPicture = pictureTemplate.cloneNode(true);
     newPicture.querySelector('.picture__img').src = picture.url;
-    newPicture.querySelector('.picture__comments').textContent = picture.commets.length;
+    newPicture.querySelector('.picture__comments').textContent = picture.comments.length;
     newPicture.querySelector('.picture__likes').textContent = picture.likes;
     return newPicture;
   };
 
-  utils.addElementsToPage(window.picturesArray, picturesSection, renderPicture);
+  var loadDataHandler = function (data) {
+    utils.addElementsToPage(data, picturesSection, renderPicture);
+    window.picturesArray = data;
+  };
+
+  dbRequests.load(loadDataHandler);
 })();
--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,13 @@
 'use strict';
 (function () {
+  var CONST = window.CONST;
+
   var fragment = document.createDocumentFragment();
+  var main = document.querySelector('main');
+  var errorTemplate = document.querySelector('#error')
+      .content
+      .querySelector('.error');
+
   window.utils = {
     getRandomValue: function (min, max) {
       return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -11,6 +18,20 @@
       }
       pageTarget.appendChild(fragment);
     },
+    errorHandler: function (errorMessage) {
+      var newError = errorTemplate.cloneNode(true);
+      newError.querySelector('.error__title').textContent = errorMessage;
+      fragment.appendChild(newError);
+      main.appendChild(fragment);
+    },
+    toSplitCommentsArray: function (array) {
+      var size = CONST.COMMENTS_PART_SIZE;
+      var subarray = [];
+      for (var i = 0; i < Math.ceil(array.length / size); i++) {
+        subarray[i] = array.slice((i * size), (i * size) + size);
+      }
+      return subarray;
+    }
   };
 })();
 
