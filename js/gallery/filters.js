034e774c8e313b600fb7c111cc2411a56c33c2ac
jinonensk <jinonensk@gmail.com>
2019-10-26 04:00:20 +0700

    add pictures filtes

---
+++ js/gallery/filters.js
@@ -0,0 +1,44 @@
+'use strict';
+
+(function () {
+  var PICTURES_FILTERS = window.CONST.PICTURES_FILTERS;
+  var gallery = window.gallery;
+  var utils = window.utils;
+
+  var makeRandomPicturesArray = function () {
+    var indexesArray = utils.getRandomIndexFromArray(PICTURES_FILTERS.QUANTITY_IN_RANDOM, window.picturesArray);
+    var newPicturesArray = [];
+    for (var i = 0; i < indexesArray.length; i++) {
+      newPicturesArray.push(window.picturesArray[indexesArray[i]]);
+    }
+    return newPicturesArray;
+  };
+
+  var throttoledfiltersHandler = function (evt) {
+    utils.throttle(function () {
+      if (!evt.target.classList.contains('img-filters__button--active')) {
+        document.querySelector('.img-filters__button--active').classList.remove('img-filters__button--active');
+        evt.target.classList.add('img-filters__button--active');
+      }
+      switch (evt.target.id) {
+        case PICTURES_FILTERS.RANDOM_BUTTON_ID:
+          gallery.clearGallery();
+          gallery.filterPicturesHandler(makeRandomPicturesArray());
+          break;
+        case PICTURES_FILTERS.DISCUSSED_BUTTON_ID:
+          gallery.clearGallery();
+          gallery.filterPicturesHandler(utils.sortItemsByField(window.picturesArray, PICTURES_FILTERS.FIELD_NAME_IN_DISCUSSED));
+          break;
+        default:
+          gallery.clearGallery();
+          gallery.filterPicturesHandler(window.picturesArray);
+          break;
+      }
+    }, PICTURES_FILTERS.DEBOUNCE_INTERVAL);
+  };
+
+  var filters = gallery.picturesFilters.querySelectorAll('.img-filters__button');
+  filters.forEach(function (item) {
+    item.addEventListener('click', throttoledfiltersHandler);
+  });
+})();
--- js/utils.js
+++ js/utils.js
@@ -2,6 +2,7 @@
 (function () {
   var CONST = window.CONST;
   var fragment = document.createDocumentFragment();
+  var time = Date.now();
 
   window.utils = {
     getRandomValue: function (min, max) {
@@ -20,7 +21,29 @@
         subarray[i] = array.slice((i * size), (i * size) + size);
       }
       return subarray;
-    }
+    },
+    getRandomIndexFromArray: function (quantity, array) {
+      var result = [];
+      while (result.length < quantity) {
+        var randomIndex = window.utils.getRandomValue(0, array.length - 1);
+        if (result.indexOf(randomIndex === -1)) {
+          result.push(randomIndex);
+        }
+      }
+      return result;
+    },
+    throttle: function (func, interval) {
+      if ((time + interval - Date.now()) < 0) {
+        func();
+        time = Date.now();
+      }
+    },
+    sortItemsByField: function (array, fieldName) {
+      var copy = array.slice();
+      return copy.sort(function (item, nextItem) {
+        return (item[fieldName] < nextItem[fieldName] ? 1 : -1);
+      });
+    },
   };
 })();
 
